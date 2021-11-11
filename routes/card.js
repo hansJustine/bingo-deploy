8acf46d0bd2dc9bac9a7bea41a0a44d7b9d2b957
Hans <sanpascualh73@gmail.com>
2021-11-11 09:19:15 +0800

    fix problem 1

--- routes/card.js
+++ routes/card.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const { createBingoCard, bingoCardForm } = require('../controllers/cards');
 
 router.get('/', bingoCardForm);
 
 router.post('/', createBingoCard);
-
 
 module.exports = router;
