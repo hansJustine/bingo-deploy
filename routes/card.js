41079138db8e2afca1f6677f441da3495bda83c2
Hans <sanpascualh73@gmail.com>
2021-11-12 23:25:24 +0800

    problem debugged 1

--- routes/card.js
+++ routes/card.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const { createBingoCard, bingoCardForm, createGameSession } = require('../controllers/cards');
 const catchAsync = require('../utils/catchAsync');
 const { validateCard, validateGameSession } = require('../middleware');
